118fbfd6afff729b0a3dfebff64fe980a013e33e
Richard Zou <richardzou@college.harvard.edu>
2014-09-17 20:05:00 -0400

    stuff

--- randmst.cpp
+++ randmst.cpp
@@ -13,6 +13,11 @@ int main(int argc, char **argv)
 {
     srand (time(NULL));
 
+    if (argc <= 5) {
+        cout << "4 arguments required. Program exiting." << endl;
+        return 1;
+    }
+
     int numpoints = atoi(argv[2]);
     int numtrials = atoi(argv[3]);
     int dimension = atoi(argv[4]);
@@ -20,22 +25,26 @@ int main(int argc, char **argv)
     for (int i = 0; i < numtrials; i++)
     {
         vector <Vertex*> setOfPoints(numpoints);
+        cout << "Creating points" << endl;
         setOfPoints = createPoints(numpoints, dimension);
 
          
         //make a complete Graph
         Graph* completeGraph = new Graph(numpoints);
+        cout << "Creating complete graph" << endl;
         addTheEdges(setOfPoints, numpoints, dimension, completeGraph);
 
-        //the answer graph
+
+        cout << "Running Kruskal's algorithm" << endl;
         Graph* answer = kruskal(completeGraph, numpoints);
 
         //this doesn't really work right now
         //printAnswer(answer->adjacency, numpoints);
 
-        cout << "The total weight is ";
+        cout << "The total weight is " << endl;
         cout << answer->totalWeight() << endl;
 
+        cout << "Cleaning up memory" << endl;
         // cleanup memory
         for (unsigned int i = 0; i < setOfPoints.size(); i++) {
             delete(setOfPoints[i]);
